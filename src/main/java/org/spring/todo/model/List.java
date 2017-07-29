52af37b348eee15515505b20cb2e3742c62077ef
cuzok <cuzook@gmail.com>
2017-07-29 21:10:50 +0530

    Added hibernate conf in pom .

---
+++ src/main/java/org/spring/todo/model/List.java
@@ -0,0 +1,34 @@
+package org.spring.todo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class List {
+	
+	@Id
+	private int id;
+	private String content;
+	private boolean isFlag;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public boolean isFlag() {
+		return isFlag;
+	}
+	public void setFlag(boolean isFlag) {
+		this.isFlag = isFlag;
+	}
+	
+
+}
