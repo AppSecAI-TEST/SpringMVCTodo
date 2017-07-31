bc4ade86e5c95c941e0ba3c0f7f7c3c3554ddae8
cuzok <cuzook@gmail.com>
2017-08-01 02:15:11 +0530

    ADD: hibernate config file

--- src/main/java/org/spring/todo/controller/TodoController.java
+++ src/main/java/org/spring/todo/controller/TodoController.java
@@ -1,24 +1,29 @@
 package org.spring.todo.controller;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spring.todo.model.TodoList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@ComponentScan("org.spring.todo.controller")
 public class TodoController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(TodoController.class); 
-	@Autowired
-	TodoList list;
-	
+//	@Autowired
+//	TodoList list;
+//	@Autowired
+//	private SessionFactory sf;
 
 	@RequestMapping("viewtodo")
 	public String showList(){
 		logger.info("Method showList()");
-	
+//	Session session = sf.getCurrentSession();
 		return "home";
 		
 	}
--- src/main/java/org/spring/todo/dao/ListDAOImpl.java
+++ src/main/java/org/spring/todo/dao/ListDAOImpl.java
@@ -3,30 +3,28 @@ package org.spring.todo.dao;
 import java.util.List;
 
 import org.spring.todo.model.TodoList;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ListDAOImpl implements ListDAO {
 
 	@Override
 	public List<TodoList> getAll() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void addList(TodoList list) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void deleteList(int id) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void updateList(int id) {
-		// TODO Auto-generated method stub
 
 	}
 
